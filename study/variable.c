c8156bb1e284a756c753f8a3a67c8792ccb3eccd
seonhyeokjun <tjsrmatn@naver.com>
2023-04-30 22:32:01 +0900

    C 언어 - 02. 변수와 상수

--- study/variable.c
+++ study/variable.c
@@ -1,42 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-//int a= 5;
+int a;
 
-/*void changeValue(){
-    //a = 10;
-}*/
-
-/*void process(){
-    static int a = 5;
-    a += 1;
-    printf("%d\n", a);
-}*/
-
-/*
-void add(int *a){
-    (*a) += 10;
-}
-*/
-
-int variable(){
-    int a = 7;
-    //add(&a);
-    printf("%d\n", a);
-    /*register int a = 10, i;
-    for (i = 0; i < a; i++) {
-        printf("%d ", i);
-    }*/
-    /*process();
-    process();
-    process();*/
-    /*int a = 7;
-    if (1){
-        a = 5;
-    }
-    printf("%d\n", a);*/
-   /* printf("%d\n", a);
-    changeValue();
-    printf("%d\n", a);*/
+int main(void){
+//    int a = 7;
+    printf("The number is %d\n", a);
+    system("pause");
     return 0;
 }
 
