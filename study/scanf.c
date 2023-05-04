6017e05ce565ed54b3c20eb7f32388a8ab820af6
seonhyeokjun <tjsrmatn@naver.com>
2023-05-04 22:32:01 +0900

    C 언어 - 03. 기본 입출력

---
+++ study/scanf.c
@@ -0,0 +1,16 @@
+#define CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+//    double a;
+//    scanf("%lf", &a);
+//    printf("%.2f\n", a);
+//    system("pause");
+
+    int a, b;
+    scanf("%1d %1d", &a, &b);
+    printf("%d %d\n", b, a);
+
+    return 0;
+}
