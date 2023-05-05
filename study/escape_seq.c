bc4d2d79af68decc3f5c1682912034343e1e91b7
seonhyeokjun <tjsrmatn@naver.com>
2023-05-05 22:32:01 +0900

    C 언어 - 04. 연산자

--- study/escape_seq.c
+++ study/escape_seq.c
@@ -1,17 +1,25 @@
+#define CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 
-int escape(){
-    int a = 7, b = 7;
-    printf("%d\n", (a == b) ? 100 : -100);
-    /*printf("%d\n", ++a);
-    printf("%d\n", a++);
-    printf("%d\n", ++a);*/
-    /*scanf("%d %d", &a, &b);
+int main(){
+//    printf("\"A \tB \tC \tD \"\n\n");
+
+    int a, b, c;
+//    scanf("%d %d", &a, &b);
+//    printf("%d\n", a > b);
+    scanf("%d %d %d", &a, &b, &c);
     printf("%d\n", !a);
     printf("%d\n", a && b);
-    printf("%d\n", (a > b) && (b > c));*/
-    //printf("%d\n", a > b);
-    //printf("\"A\tB\tC\tD\"\n");
+    printf("%d\n", (a > b) && (b > c));
+
+    int d = 7, e = 7;
+    printf("%d\n", ++d);
+    printf("%d\n", d++);
+    printf("%d\n", ++d);
+    printf("%d\n", (d == e) ? 100 : -100);
+
+    system("pause");
     return 0;
 }
 
---
+++ study/operator.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d + %d = %d\n", a, b, a + b);
+    printf("%d - %d = %d\n", a, b, a - b);
+    printf("%d * %d = %d\n", a, b, a * b);
+    printf("%d / %d = %d\n", a, b, a / b);
+    printf("%d %% %d = %d\n", a, b, a % b);
+    system("pause");
+    return 0;
+}
